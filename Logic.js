06a651563be0ded62fdd68804a7bf2385bf582af
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-04-09 23:43:02 +0530

    Update Logic.js

--- Logic.js
+++ Logic.js
@@ -111,3 +111,37 @@ findLongestWord(['banana','three','abcd'])
 
 qustions
 
+Nested Functions and Context
+Task 1: Using this in Objects
+
+Create an object person with a method introduce() that uses this
+additionally add properties of name & age that will result in Hi, my name is Hitesh and I am 19.5 years old on calling introduce()
+
+
+Task 2: Function within a function
+Write a function outer() that contains another function inner() and returns a value of 'Inner function called' on calling outer()
+
+answers
+
+task 1
+const person = {
+    name: 'Hitesh',
+    age: 19.5,
+    introduce : function () {return `Hi, my name is ${this.name} and I am ${this.age} years old`}
+};
+
+
+task 2
+function outer() {
+   let lat
+   
+    function inner(){
+         lat = 'Inner function called' 
+    }
+    inner()
+    
+    return lat
+}
+
+outer()
+    
